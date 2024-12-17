c773572c505e4f3e70bc988ced770f191cebe98a
Saarthakkj <saarthak22421@iiitd.ac.in>
2024-12-17 21:08:57 +0530

    changed only github link

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const dialogueData = {
   "my-cupboard": "this is my cupboard",
   "roomate-cupboard": 'this is cupboard',
   "my-pc": `This is my PC. Anyway, regardless of the language, I just like programming.
-    Here is my <a href="https://saarthaksaxena.tech/careers" target="_blank">Github</a>!`,
+    Here is my <a href="https://github.com/Saarthakkj" target="_blank">Github</a>!`,
     "roomate-bed" : "this is roomate bed",
     "my-bed" : "this is my bed",
   "bed": `This is where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
